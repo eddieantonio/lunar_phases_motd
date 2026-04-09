0fd4f94caff81d7f3c19786f83dde00497a0a1a6
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2026-04-08 20:38:21 -0600

    Suggestion 3: remove redundant comparisons

--- lunar_phases.py
+++ lunar_phases.py
@@ -118,30 +118,30 @@ day2 = "day" if cntdwn2 == 1 else "days"
 
 # Assigning a physical phase to the calculated date 
 # printing ASCII, phase name, and countdown to next new/full moon
-if 0 < phase <= 6.375 :
+if phase <= 6.375 :
     print(phases_dict["WAXING CRESCENT"])
     print("Today the moon is a WAXING CRESCENT")
     print(f"{cntdwn} {day} until next Full Moon")
-elif 6.375 < phase <= 7.375 :
+elif phase <= 7.375 :
     print(phases_dict["FIRST QUARTER"])
     print("Today the moon is starting its FIRST QUARTER")
     print(f"{cntdwn} {day} until next Full Moon")
-elif 7.375 < phase <= 13.75 :
+elif phase <= 13.75 :
     print(phases_dict["WAXING GIBBOUS"])
     print("Today the moon is a WAXING GIBBOUS")
     print(f"{cntdwn} {day} until next Full Moon")
-elif 13.75 < phase <= 14.75 :
+elif phase <= 14.75 :
     print(phases_dict["FULL MOON"])
     print("Today is the FULL MOON!")
-elif 14.75 < phase <= 21.125 :
+elif phase <= 21.125 :
     print(phases_dict["WANING GIBBOUS"])
     print("Today the moon is a WANING GIBBOUS")
     print(f"{cntdwn2} {day2} until next New Moon")
-elif 21.125 < phase <= 22.125 :
+elif phase <= 22.125 :
     print(phases_dict["LAST QUARTER"])
     print("Today the moon is starting its LAST QUARTER")
     print(f"{cntdwn2} {day2} until next New Moon")
-elif 22.125 < phase <= 28.5 :
+elif phase <= 28.5 :
     print(phases_dict["WANING CRESCENT"])
     print("Today the moon is a WANING CRESCENT")
     print(f"{cntdwn2} {day2} until next New Moon")
