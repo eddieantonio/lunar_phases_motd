56c67d059267bfc5f70b3519b9bcec8c641036d4
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2026-04-08 19:52:16 -0600

    refactor: metaclass over-engineering

--- lunar_phases.py
+++ lunar_phases.py
@@ -1,5 +1,6 @@
 import time 
 import math
+from textwrap import dedent
 
 SECONDS_IN_DAY = 86400
 JULIAN_DATE_UNIX_EPOCH = 2440587.5
@@ -14,80 +15,135 @@ def lunar_phase():
     return phase_calculation
 
 
-phases_dict = {
-"NEW MOON" : 
-"""
-       _..._     
-     .'     `.    
-    :         :    
-    :         :  
-    `.       .'  
-jgs   `-...-'  
-""", 
-"WAXING CRESCENT" : 
-"""
-       _..._     
-     .'   `::.    
-    :       :::    
-    :       :::  
-    `.     .::'  
-jgs   `-..:'' 
-""", 
-"FIRST QUARTER" : 
-"""
-       _..._     
-     .'  ::::.    
-    :    ::::::    
-    :    ::::::  
-    `.   :::::'  
-jgs   `-.::''   
-""", 
-"WAXING GIBBOUS" : 
-""" 
-       _..._     
-     .' .::::.    
-    :  ::::::::    
-    :  ::::::::  
-    `. '::::::'  
-jgs   `-.::''  
-""", 
-"FULL MOON" : 
-"""    
-       _..._     
-     .:::::::.    
-    :::::::::::   
-    ::::::::::: 
-    `:::::::::'  
-jgs   `':::'' 
-""", 
-"WANING GIBBOUS" : 
-""" 
-       _..._     
-     .::::. `.    
-    :::::::.  :    
-    ::::::::  :  
-    `::::::' .'  
-jgs   `'::'-'
- """, 
- "LAST QUARTER" : 
- """
-       _..._     
-     .::::  `.    
-    ::::::    :    
-    ::::::    :  
-    `:::::   .'  
-jgs   `'::.-'   
-""", 
-"WANING CRESCENT" : 
-"""  
-       _..._     
-     .::'   `.    
-    :::       :    
-    :::       :  
-    `::.     .'  
-jgs   `':..-'
-"""
-}
+PHASES = []
+
+class MoonPhaseMeta(type):
+    """
+    Metaclass automatically processes the name and __doc__.
+    """
+    def __new__(cls, name, bases, dct):
+        phase = super().__new__(cls, name, bases, dct)
+        phase.name = camel_case_to_capitalized_string(name)
+        phase.ascii_art = dedent(dct["__doc__"])
+        phase.today_is = "normal"
+        phase.countdown = None
+        PHASES.append(phase)
+        return phase
+
+
+def camel_case_to_capitalized_string(name):
+    for i, c in enumerate(name[1:]):
+        if 'A' <= c <= 'Z':
+            # Account for chopping of the first number of this loop:
+            split_pos = i + 1
+            break
+    return name[:split_pos] + " " + name[split_pos:]
+
+
+class WaxingCrescent(metaclass=MoonPhaseMeta):
+    """
+           _..._     
+         .'   `::.    
+        :       :::    
+        :       :::  
+        `.     .::'  
+    jgs   `-..:'' 
+    """
+    upper_boundary = 6.375
+    countdown = "to-full-moon"
+
+class FirstQuarter(metaclass=MoonPhaseMeta):
+    """
+           _..._     
+         .'  ::::.    
+        :    ::::::    
+        :    ::::::  
+        `.   :::::'  
+    jgs   `-.::''   
+    """
+    upper_boundary = 7.375
+    countdown = "to-full-moon"
+    today_is = "quarter"
+
+class WaxingGibbous(metaclass=MoonPhaseMeta):
+    """ 
+           _..._     
+         .' .::::.    
+        :  ::::::::    
+        :  ::::::::  
+        `. '::::::'  
+    jgs   `-.::''  
+    """
+    upper_boundary = 13.75
+    countdown = "to-full-moon"
+
+
+class FullMoon(metaclass=MoonPhaseMeta):
+    """
+           _..._     
+         .:::::::.    
+        :::::::::::   
+        ::::::::::: 
+        `:::::::::'  
+    jgs   `':::'' 
+    """
+    upper_boundary = 14.75
+    action = "full-moon"
+    today_is = "special"
+
+
+class WaningGibbous(metaclass=MoonPhaseMeta):
+    """ 
+           _..._     
+         .::::. `.    
+        :::::::.  :    
+        ::::::::  :  
+        `::::::' .'  
+    jgs   `'::'-'
+     """
+    upper_boundary = 21.125
+    countdown = "to-new-moon"
+
+
+class LastQuarter(metaclass=MoonPhaseMeta):
+    """
+           _..._     
+         .::::  `.    
+        ::::::    :    
+        ::::::    :  
+        `:::::   .'  
+    jgs   `'::.-'   
+    """
+    upper_boundary = 22.125
+    countdown = "to-new-moon"
+    today_is = "quarter"
+
+
+class WaningCrescent(metaclass=MoonPhaseMeta):
+    """  
+           _..._     
+         .::'   `.    
+        :::       :    
+        :::       :  
+        `::.     .'  
+    jgs   `':..-'
+    """
+    upper_boundary = 28.5
+    countdown = "to-new-moon"
+
+
+class NewMoon(metaclass=MoonPhaseMeta):
+    """
+           _..._     
+         .'     `.    
+        :         :    
+        :         :  
+        `.       .'  
+    jgs   `-...-'  
+    """
+    countdown = "new-moon"
+    today_is = "special"
+
 
 # boundary points for lunar phase
 """
@@ -100,53 +156,6 @@ Last quarter: 21.125-22.125
 Waning crescent: 22.125-28.5
 New moon: 28.5-29.5
 """
-
-PHASE_INFO = [
-    {
-        "name": "WAXING CRESCENT",
-        "upper_boundary": 6.375,
-        "countdown": "to-full-moon",
-    },
-    {
-        "name": "FIRST QUARTER",
-        "upper_boundary": 7.375,
-        "countdown": "to-full-moon",
-        "today_is": "quarter"
-    },
-    {
-        "name": "WAXING GIBBOUS",
-        "upper_boundary": 13.75,
-        "countdown": "to-full-moon",
-    },
-    {
-        "name": "FULL MOON",
-        "upper_boundary": 14.75,
-        "action": "full-moon",
-        "today_is": "special"
-    },
-    {
-        "name": "WANING GIBBOUS",
-        "upper_boundary": 21.125,
-        "countdown": "to-new-moon",
-    },
-    {
-        "name": "LAST QUARTER",
-        "upper_boundary": 22.125,
-        "countdown": "to-new-moon",
-        "today_is": "quarter"
-    },
-    {
-        "name": "WANING CRESCENT",
-        "upper_boundary": 28.5,
-        "countdown": "to-new-moon",
-    },
-    {
-        "name": "NEW MOON",
-        "upper_boundary": LUNAR_PHASE_LENGTH,
-        "countdown": "new-moon",
-        "today_is": "special"
-    },
-]
 
 def days_plural(number):
     word = "day" if number == 1 else "days"
@@ -162,34 +171,28 @@ def print_motd(phase=None):
 
     # Assigning a physical phase to the calculated date 
     lo = 0
-    lo += 4 if phase >= PHASE_INFO[lo + 3]["upper_boundary"] else 0
-    lo += 2 if phase >= PHASE_INFO[lo + 1]["upper_boundary"] else 0
-    lo += 1 if phase >= PHASE_INFO[lo + 0]["upper_boundary"] else 0
-    info = PHASE_INFO[lo]
-
-    # printing ASCII, phase name, and countdown to next new/full moon
-    name = info["name"]
+    lo += 4 if phase >= PHASES[lo + 3].upper_boundary else 0
+    lo += 2 if phase >= PHASES[lo + 1].upper_boundary else 0
+    lo += 1 if phase >= PHASES[lo + 0].upper_boundary else 0
+    moon = PHASES[lo]
 
     # Print the moon!
-    ascii_art = phases_dict[name]
-    print(ascii_art)
+    print(moon.ascii_art)
 
     # Print the first line of the message:
-    today_is_style = info.get("today_is", "normal")
-    if today_is_style == "normal":
-        print(f"Today the moon is a {name}")
-    elif today_is_style == "quarter":
-        print(f"Today the moon is starting its {name}")
-    elif today_is_style == "special":
-        print(f"Today is the {name}!")
+    if moon.today_is == "normal":
+        print(f"Today the moon is a {moon.name}")
+    elif moon.today_is == "quarter":
+        print(f"Today the moon is starting its {moon.name}")
+    elif moon.today_is == "special":
+        print(f"Today is the {moon.name}!")
 
     # Print the countdown (if present)
-    countdown = info.get("countdown")
-    if countdown == "to-full-moon":
+    if moon.countdown == "to-full-moon":
         # Calculating days to next full/new moon
         cntdwn = math.floor(LUNAR_PHASE_HALFPOINT - phase)
         print(f"{days_plural(cntdwn)} until next Full Moon")
-    elif countdown == "to-new-moon":
+    elif moon.countdown == "to-new-moon":
         cntdwn2 = math.floor(LUNAR_PHASE_LENGTH - phase)
         print(f"{days_plural(cntdwn2)} until next New Moon")
 
